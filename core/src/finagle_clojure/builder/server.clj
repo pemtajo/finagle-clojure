31683f3a130048b5dac38861e6d94a18733e55cf
Sophia Velten de Melo <sophia.velten@nubank.com.br>
2022-10-05 17:04:16 -0300

    clean

--- core/src/finagle_clojure/builder/server.clj
+++ core/src/finagle_clojure/builder/server.clj
@@ -3,37 +3,27 @@
   of any particular codec. Generally speaking codec-specific server functions
   should be preferred, but these are included for comptability with older systems
   configured at the server level."
-  (:require [finagle-clojure.scala :as scala]
-            [erp12.fijit.alpha.reflect :as r])
-  (:import (com.twitter.finagle.server StdStackServer StackServer StackServer$)
+  (:require [finagle-clojure.scala :as scala])
+  (:import (com.twitter.finagle.server StackServer$)
            (com.twitter.finagle Service ListeningServer)
            (com.twitter.finagle.netty4 Netty4Listener Netty4Listener$)
            (java.net InetSocketAddress)
-           (io.netty.channel Channel ChannelPipeline ChannelFactory ChannelInitializer)
+           (io.netty.channel ChannelPipeline)
            (io.netty.handler.codec.string StringEncoder StringDecoder)
            (io.netty.handler.codec DelimiterBasedFrameDecoder Delimiters)
            (io.netty.util CharsetUtil)
-           (scala.reflect Manifest Manifest$)
-           (com.twitter.util Duration Future)
-           (com.twitter.finagle.tracing Tracer)
-           (com.twitter.finagle.dispatch SerialServerDispatcher)
-           (com.twitter.finagle.stats StatsReceiver)
-           (com.twitter.finagle.server StackBasedServer)))
+           (scala.reflect Manifest)
+           (com.twitter.util Future)
+           (com.twitter.finagle.dispatch SerialServerDispatcher)))
 
-(defn string-pipeline-builder
+(defn- string-pipeline-builder
   [^ChannelPipeline p]
   (doto p
     (.addLast "line" (new DelimiterBasedFrameDecoder 100 (Delimiters/lineDelimiter)))
     (.addLast "stringDecoder" (new StringDecoder (CharsetUtil/UTF_8)))
     (.addLast "stringEncoder" (new StringEncoder (CharsetUtil/UTF_8)))))
 
-(defn pipeline-initializer
-  [pipeline-builder]
-  (proxy [ChannelInitializer] []
-    (initChannel [^Channel ch]
-      (pipeline-builder ^ChannelPipeline (.pipeline ch)))))
-
-(defn ^Netty4Listener netty4listener
+(defn- ^Netty4Listener netty4listener
   []
   (.apply Netty4Listener$/MODULE$
           (scala/Function [p] (string-pipeline-builder p))
@@ -41,13 +31,18 @@
           (Manifest/Any)
           (Manifest/Any)))
 
-(defn dispatcher-builder
-  [service]
+(defn- dispatcher-builder
+  [^Service service]
   (scala/Function [t] (SerialServerDispatcher. t service)))
 
-(defn address
+(defn- address
   [p]
   (InetSocketAddress. (int p)))
+
+(defn ^ListeningServer server
+  [service port]
+  (let [server (.listen (netty4listener) (address port) (dispatcher-builder service))]
+    server))
 
 (defn ^Future close!
   "Stops the given Server.
